87669028acd18b6de8382c03037811432ad3fd7e
Arjun <arjun2000raj@gmail.com>
2020-07-28 00:32:17 +0530

    Updates

---
+++ precache-manifest.30430ec10ced3e14ede8d21f5dd35311.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "685c8d8737e06dc393df1ca9fbc55fb1",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "5c4bfbb91f0257951d94",
+    "url": "/To-Do-List/static/css/main.79ef6309.chunk.css"
+  },
+  {
+    "revision": "3f76bfd968581874d475",
+    "url": "/To-Do-List/static/js/2.ff977aae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-List/static/js/2.ff977aae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c4bfbb91f0257951d94",
+    "url": "/To-Do-List/static/js/main.4421edf1.chunk.js"
+  },
+  {
+    "revision": "3fc2ca48924706e73961",
+    "url": "/To-Do-List/static/js/runtime-main.ce724f51.js"
+  }
+]);
